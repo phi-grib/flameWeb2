739dc9c9ce1c42e3002c7adc7137ef4bd39c480f
IgnacioPasamontes <ignacio.pasamontes@upf.edu>
2020-02-28 12:50:36 +0100

    Last version

--- src/app/quantit-no-conformal/quantit-no-conformal.component.ts
+++ src/app/quantit-no-conformal/quantit-no-conformal.component.ts
@@ -20,9 +20,15 @@ export class QuantitNoConformalComponent implements OnChanges {
   @Input() modelName;
   @Input() modelVersion;
   modelDocumentation = undefined;
-  orderDocumentation = ['ID', 'Version', 'Contact', 'Institution', 'Date', 'Endpoint', 'Endpoint_units', 'Dependent_variable', 'Species',
-            'Limits_applicability', 'Experimental_protocol', 'Data_info', 'Model_availability', 'Algorithm', 'Software', 'Descriptors',
-            'Algorithm_settings', 'AD_method', 'AD_parameters', 'Goodness_of_fit_statistics', 'Internal_validation_1' ];
+  orderDocumentation = ['ID', 'Version', 'Contact', 'Institution', 'Date', 'Endpoint',
+  'Endpoint_units', 'Interpretation', 'Dependent_variable', 'Species',
+ 'Limits_applicability', 'Experimental_protocol', 'Model_availability',
+ 'Data_info', 'Algorithm', 'Software', 'Descriptors', 'Algorithm_settings',
+ 'AD_method', 'AD_parameters', 'Goodness_of_fit_statistics', 
+ 'Internal_validation_1', 'Internal_validation_2', 'External_validation',
+ 'Comments', 'Other_related_models', 'Date_of_QMRF', 'Data_of_QMRF_updates',
+ 'QMRF_updates', 'References', 'QMRF_same_models', 'Comment_on_the_endpoint',
+ 'Endpoint_data_quality_and_variability', 'Descriptor_selection'];
 
   objectKeys = Object.keys;
   modelBuildInfo = {};
