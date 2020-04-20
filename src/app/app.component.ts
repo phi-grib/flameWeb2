d948f9488e2a3ac132323532f19834f9aaf10946
manuelpastor <manuel.pastor@upf.edu>
2020-04-20 12:41:15 +0200

    improved plotly support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,18 +9,16 @@ import { Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'flameweb';
-
+  
   constructor(public prediction: Prediction,
     public model: Model,
     private router: Router) {}
-
+    
     isActive(url: string) {
       return this.router.url.includes(url);
- 
-   }
- 
+    }
 }
-
-
