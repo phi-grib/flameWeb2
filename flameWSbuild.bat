8a562a3620a81c2eceb67cb8214ba2f5cedd6248
manuelpastor <manuel.pastor@upf.edu>
2020-05-02 20:20:44 +0200

    improved placement of images

--- flameWSbuild.bat
+++ flameWSbuild.bat
@@ -1,4 +1,4 @@
 x:
 cd \soft\flameWeb2
-@CALL ng build --deploy-url /static/  --output-path=../flame_API/flame_api/static/
+@CALL ng build --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
 timeout /t 10 /nobreak > NUL
