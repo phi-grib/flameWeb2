451bec5ab2d71eff55d516fa83d05604cb6dc109
manuelpastor <manuel.pastor@upf.edu>
2020-05-09 20:36:45 +0200

    improved series plots

--- src/app/quantit-conformal/quantit-conformal.component.ts
+++ src/app/quantit-conformal/quantit-conformal.component.ts
@@ -102,16 +102,31 @@ export class QuantitConformalComponent implements OnChanges {
           type: 'scatter', 
           mode: 'markers', 
           marker: {
-            color: 'rgba(255,0,0,0.2)',
-            size: 12,
-            line: {
-              color: 'red',
-              width: 2
+            color: [],
+            opacity: 0.6,
+            colorscale: 'RdBu', 
+            showscale: true, 
+            cmax: 1.0,
+            cmin: 0.0,
+            size: 14,
+            // line: {
+            //   width: 2
+            // },
+            colorbar: {
+              tickfont: {
+                family: 'Barlow Semi Condensed, sans-serif',
+                size: 20
+              }
             }
           },
+          hovertemplate:'Activity: %{marker.color:.2f}<br><b>%{text}</b>',
         }
       ],
     }
+
+    // hovertemplate: '<i>Price</i>: $%{y:.2f}' +
+    //                     '<br><b>X</b>: %{x}<br>' +
+    //                     '<b>%{text}</b>',
 
     plotCommon = {
       layout: { 
@@ -171,6 +186,7 @@ export class QuantitConformalComponent implements OnChanges {
         layout: { 
           width: 950,
           height: 600,
+          hovermode: 'closest',
           margin: {
             r: 10,
             t: 30,
@@ -180,7 +196,7 @@ export class QuantitConformalComponent implements OnChanges {
           showtitle: false,
           xaxis: {
             hoverformat: '.2f',
-            zeroline: false,
+            zeroline: true,
             showgrid: true,
             showline: true,
             gridwidth: 1,
@@ -198,7 +214,7 @@ export class QuantitConformalComponent implements OnChanges {
           },
           yaxis: {
             hoverformat: '.2f',
-            zeroline: false,
+            zeroline: true,
             showgrid: true,
             showline: true,
             gridwidth: 1,
@@ -218,7 +234,7 @@ export class QuantitConformalComponent implements OnChanges {
       config: {
         // responsive: true,
         displaylogo: false,
-        modeBarButtonsToRemove: ['lasso2d', 'select2d', 'autoScale2d']    }
+        modeBarButtonsToRemove: ['lasso2d', 'select2d', 'autoScale2d','hoverCompareCartesian']    }
     };
       
     ngOnChanges(): void {
@@ -239,6 +255,7 @@ export class QuantitConformalComponent implements OnChanges {
       this.plotScores.data[0].x =[];
       this.plotScores.data[0].y =[];
       this.plotScores.data[0].text =[];
+      this.plotScores.data[0].marker.color = [];
       
       this.getValidation();
     }
@@ -282,6 +299,13 @@ export class QuantitConformalComponent implements OnChanges {
               this.plotScores.data[0].x = info['PC1'];
               this.plotScores.data[0].y = info['PC2'];
               this.plotScores.data[0].text = info['obj_nam'];
+              // this.plotScores.data[0].meta = info['ymatrix'];
+              // var scale = [];
+              var min = Math.min.apply(Math, info['ymatrix']);
+              var max = Math.max.apply(Math, info['ymatrix'])
+              this.plotScores.data[0].marker.cmin = min;
+              this.plotScores.data[0].marker.cmax = max;
+              this.plotScores.data[0].marker.color = info['ymatrix'];
             }
 
             // predicted data
