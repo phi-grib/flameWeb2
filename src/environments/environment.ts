8aff4923674b660c677c5806dcb4eaaddc20e3ac
Manuel Pastor <manuel.pastor@upf.edu>
2020-05-29 18:37:30 +0200

    keycloak configuration revision

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,19 +5,22 @@
 import { KeycloakConfig } from 'keycloak-angular';
 
  // Add here your keycloak setup infos
- const keycloakConfig: KeycloakConfig = {
-  url: 'KEYCLOAK-INSTANCE-URL',
-  realm: 'REALM-NAME',
-  clientId: 'CLIENT-ID-NAME'
+const keycloakConfig: KeycloakConfig = {
+  url: 'https://login.etransafe.eu/auth',
+  realm: 'KH',
+  clientId: 'flame'
 };
 
-
 export const environment = {
-
-
-
   keycloakConfig,
   production: false,
+  // baseUrl: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1',
+  // baseUrl_manage: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/manage/',
+  // baseUrl_build: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/build/',
+  // baseUrl_predict: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/predict/',
+  // baseUrl_smanage: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/smanage/',
+  // baseUrl_sbuild: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/sbuild/',
+  // baseUrl_search: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/search/'
   baseUrl: 'http://localhost:8000/api/v1',
   baseUrl_manage: 'http://localhost:8000/api/v1/manage/',
   baseUrl_build: 'http://localhost:8000/api/v1/build/',
