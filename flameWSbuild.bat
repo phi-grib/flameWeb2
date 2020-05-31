fc337bf500ae9edc94b54c31e2922df5436a9106
Manuel Pastor <manuel.pastor@upf.edu>
2020-05-31 11:34:33 +0200

    improvement of Windows building tools (local and containers)

--- flameWSbuild.bat
+++ flameWSbuild.bat
@@ -1,4 +1,6 @@
 x:
 cd \soft\flameWeb2
-@CALL ng build --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
+copy \soft\flameWeb2\src\environments\loc-environment.ts \soft\flameWeb2\src\environments\environment.ts
+copy \soft\flameWeb2\src\environments\loc-environment.prod.ts \soft\flameWeb2\src\environments\environment.prod.ts
+@CALL ng build --prod --optimization=false --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
 timeout /t 10 /nobreak > NUL
