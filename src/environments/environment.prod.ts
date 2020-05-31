d444ffb8b96340430d47d357d8910dd0e459dc98
Manuel Pastor <manuel.pastor@upf.edu>
2020-05-31 12:19:31 +0200

    solves isssue with keycloak.json file not found

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,11 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1',
-  baseUrl_manage: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/manage/',
-  baseUrl_build: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/build/',
-  baseUrl_predict: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/predict/',
-  baseUrl_smanage: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/smanage/',
-  baseUrl_sbuild: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/sbuild/',
-  baseUrl_search: 'https://dev.kh.etransafe.eu/flame.kh.svc/api/v1/search/'
+
+  baseUrl: 'http://localhost:8000/flame.kh.svc/api/v1',
+  baseUrl_manage: 'http://localhost:8000/flame.kh.svc/api/v1/manage/',
+  baseUrl_build: 'http://localhost:8000/flame.kh.svc/api/v1/build/',
+  baseUrl_predict: 'http://localhost:8000/flame.kh.svc/api/v1/predict/',
+  baseUrl_smanage: 'http://localhost:8000/flame.kh.svc/api/v1/smanage/',
+  baseUrl_sbuild: 'http://localhost:8000/flame.kh.svc/api/v1/sbuild/',
+  baseUrl_search: 'http://localhost:8000/flame.kh.svc/api/v1/search/'
 };
