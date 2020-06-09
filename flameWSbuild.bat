a1edbeedbf1d1c780d8cd6d4747878d9f8a7c95b
Manuel Pastor <manuel.pastor@upf.edu>
2020-06-09 20:08:10 +0200

    improved handling of environments

--- flameWSbuild.bat
+++ flameWSbuild.bat
@@ -1,6 +1,4 @@
 x:
 cd \soft\flameWeb2
-copy \soft\flameWeb2\src\environments\loc-environment.ts \soft\flameWeb2\src\environments\environment.ts
-copy \soft\flameWeb2\src\environments\loc-environment.prod.ts \soft\flameWeb2\src\environments\environment.prod.ts
-@CALL ng build --prod --optimization=false --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
-timeout /t 10 /nobreak > NUL
+@CALL ng build --prod --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
+timeout /t 20 /nobreak > NUL
--- flameWScontainer.bat
+++ flameWScontainer.bat
@@ -1,7 +1,5 @@
 x:
 cd \soft\flameWeb2
-copy \soft\flameWeb2\src\environments\th-environment.ts \soft\flameWeb2\src\environments\environment.ts
-copy \soft\flameWeb2\src\environments\th-environment.prod.ts \soft\flameWeb2\src\environments\environment.prod.ts
-@CALL ng build --base-href /flame.kh.svc/ --deploy-url /flame.kh.svc/static/ --output-path=../flame_API/flame_api/static/
 xcopy \soft\flame_API\flame_api \soft\FlameDocker\flame_api /e /y
-timeout /t 10 /nobreak > NUL
+@CALL ng build --configuration=kh --base-href /flame.kh.svc/ --deploy-url /flame.kh.svc/static/ --output-path=../FlameDocker/flame_api/static/
+timeout /t 20 /nobreak > NUL
