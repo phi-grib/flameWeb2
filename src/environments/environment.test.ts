0283d5cd1e9c671c5185a736b2117608542330f1
Manuel Pastor <manuel.pastor@upf.edu>
2020-09-07 18:27:49 +0200

    added support for dev.kh.etransafe.eu

---
+++ src/environments/environment.test.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  baseUrl: 'https://test.kh.etransafe.eu/flame.kh.svc/api/v1',
+  baseUrl_manage: 'https://test.kh.etransafe.eu/flame.kh.svc/api/v1/manage/',
+  baseUrl_build: 'https://test.kh.etransafe.eu/flame.kh.svc/api/v1/build/',
+  baseUrl_predict: 'https://test.kh.etransafe.eu/flame.kh.svc/api/v1/predict/',
+  baseUrl_smanage: 'https://test.kh.etransafe.eu/flame.kh.svc/api/v1/smanage/',
+  baseUrl_sbuild: 'https://test.kh.etransafe.eu/flame.kh.svc/api/v1/sbuild/',
+  baseUrl_search: 'https://test.kh.etransafe.eu/flame.kh.svc/api/v1/search/'
+};
