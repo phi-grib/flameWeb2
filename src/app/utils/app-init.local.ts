4451df18a769e4493e2dca2eba2133f29a6996b8
Manuel Pastor <manuel.pastor@upf.edu>
2020-12-16 19:53:41 +0100

    adapted to aws and local

---
+++ src/app/utils/app-init.local.ts
@@ -0,0 +1,51 @@
+import {KeycloakConfig, KeycloakEventType, KeycloakService} from 'keycloak-angular';
+import {filter} from "rxjs/operators";
+
+// THIS WORKS WITH THE LOCAL INSTANCE OF KEYCLOACK
+// const keycloakConfig: KeycloakConfig = {
+//     url: 'http://localhost:8090/auth',
+//     realm: "KH",
+//     credentials: {
+//         secret: "**********"
+//     },
+//     clientId: "knowledge-hub"
+// };
+
+
+export function initializer(keycloak: KeycloakService): () => Promise<any> {
+    return (): Promise<any> => {
+
+        // tell keycloak to get the token once it is expired
+        // it updates the token like that but you can still have a scenario where you issue an http request
+        // and the token has not yet been updated
+        // therefore we have a
+        // await keycloak.updateToken(100) before most of the http calls in our apps
+        // I would have expected that this is something that angular-keycloak does automatically, but it doesn't seem to be the case
+        keycloak.keycloakEvents$.pipe(filter(keycloak_event => keycloak_event.type === KeycloakEventType.OnTokenExpired)).subscribe(() => {
+            console.log("Updating token");
+            keycloak.updateToken(200);
+        });
+
+        return new Promise(async (resolve, reject) => {
+            try {
+                await keycloak.init({
+                    //config: keycloakConfig,
+
+                    config: "secrets/keycloak.json",
+                    enableBearerInterceptor: true,
+                    bearerPrefix: 'Bearer', // Tima thinks capital might be important here
+
+                    // I'm not sure if the lines below are really needed
+                    initOptions: {
+                        onLoad: 'login-required',
+                        // checkLoginIframe: false
+                    }
+                    /*bearerExcludedUrls: []*/
+                });
+                resolve();
+            } catch (error) {
+                reject(error);
+            }
+        });
+    };
+}
