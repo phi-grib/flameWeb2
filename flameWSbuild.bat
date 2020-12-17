4c448223468fd380bd46519b819d88c58972d266
Manuel Pastor <manuel.pastor@upf.edu>
2020-12-17 18:33:06 +0100

    environments cleaning

--- flameWSbuild.bat
+++ flameWSbuild.bat
@@ -1,5 +1,4 @@
 x:
 cd \soft\flameWeb2
 @CALL ng build --prod --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
-REM @CALL ng build --prod --configuration=aws --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
 timeout /t 20 /nobreak > NUL
