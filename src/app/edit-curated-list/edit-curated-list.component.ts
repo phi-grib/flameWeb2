69819d3d889c186b9f56abafc1a4949dffeed0c9
Royphi <rodrigo.lorenzo.lor@gmail.com>
2021-02-19 15:26:22 +0100

    "count rows with different separators done"

--- src/app/edit-curated-list/edit-curated-list.component.ts
+++ src/app/edit-curated-list/edit-curated-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm, NgModel }  from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { Model } from '../Globals';
 
 
 
@@ -13,7 +14,8 @@ export class EditCuratedListComponent implements OnInit {
 
   ObjCuratedList: EditCuratedListComponent;
   ObjActiveModal: NgbActiveModal;
-  SeparatorChoices=[",", " ", ";", "  ", "/", "."];
+  SeparatorChoices={",":",", "Espace bar": " ", "Tab":"  ", "/": "/", ".": ".", ":": ":", ";": ";"};
+  fileContent: any;
 
   //attributes: 
   public listName: string;
@@ -23,11 +25,10 @@ export class EditCuratedListComponent implements OnInit {
   public substanceName: string;
   public fileList: File;
 
-  constructor(public activeModal: NgbActiveModal
-    ) { }
+  constructor(public activeModal: NgbActiveModal, public model: Model) { }
 
   ngOnInit(): void {
-    this.ObjCuratedList = new EditCuratedListComponent(this.ObjActiveModal)
+    this.ObjCuratedList = new EditCuratedListComponent(this.ObjActiveModal, this.model)
   }
 
   createObjCuratedList(){
@@ -35,13 +36,39 @@ export class EditCuratedListComponent implements OnInit {
   }
 
   onChangeSeparator(selectValue: any){
-    let separatorValue = "";
-    for(let item in this.SeparatorChoices){
-      if(item = selectValue){
-        separatorValue = item;
-      }
-    }
-    this.ObjCuratedList.separator = separatorValue;
+
+    this.ObjCuratedList.separator = selectValue;
   }
 
+  preloadFileFunction(event): void {
+    const file = event.target.files[0];
+    this.model.file = file;
+    this.model.file_info = {};
+    this.model.file_info['name'] = file.name;
+    this.model.file_info['size_M'] = ((file.size / 1024) / 1024).toFixed(2);
+    const extension = file.name.split('.');
+    this.model.file_info['type_file'] = extension[1];
+    const fileReader: FileReader = new FileReader();
+    const self = this;
+    fileReader.onloadend = function(x) {
+      self.fileContent = fileReader.result;
+      var re = new RegExp("((.*)"+self.ObjCuratedList.separator+"(.*)*$)","mg"); 
+      self.model.file_info['num_mols'] = (self.fileContent.match(re) || []).length -1;
+      const res_array = self.fileContent.match(/>( )*<(.*)>/g);
+      const res_dict = {};
+      for (const variable of res_array) {
+        const value = variable.replace(/[<> ]*/g, '');
+        if (value in res_dict) {
+          res_dict[value] = res_dict[value] + 1;
+        }
+        else {
+          res_dict[value] = 1;
+        }
+      }
+      self.model.file_fields = res_dict;
+    };
+    fileReader.readAsText(file);
+  }
 }
+
+
