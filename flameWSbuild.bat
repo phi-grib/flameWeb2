30e830477f05493e9acdd1d38547c7285287dd4e
Manuel Pastor <manuel.pastor@upf.edu>
2021-02-23 15:45:46 +0100

    scripts to use diverse environments

--- flameWSbuild.bat
+++ flameWSbuild.bat
@@ -1,4 +1,4 @@
 x:
 cd \soft\flameWeb2
-@CALL ng build --prod --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
+@CALL ng build --prod --configuration=build --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
 timeout /t 20 /nobreak > NUL
--- flameWScontainer-rel-key.bat
+++ flameWScontainer-rel-key.bat
@@ -2,4 +2,7 @@ x:
 cd \soft\flameWeb2
 xcopy \soft\flame_API\flame_api \soft\upf\flame_api /e /y
 @CALL ng build --prod --configuration=rel-key --base-href /flame.kh.svc/ --deploy-url /flame.kh.svc/static/ --output-path=../upf/flame_api/static/
+cd \soft\upf\flame_api\static
+move assets ..
+xcopy \soft\upf\secrets\keycloak.json \soft\upf\flame_api\secrets /e /y
 timeout /t 20 /nobreak > NUL
