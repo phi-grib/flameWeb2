db2a672f487437fced41296e0d857c2bc780f683
Manuel Pastor <manuel.pastor@upf.edu>
2021-04-14 09:39:26 +0200

    update to FlameDocker instead of upf

--- flameWScontainer-rel.bat
+++ flameWScontainer-rel.bat
@@ -1,5 +1,5 @@
 x:
 cd \soft\flameWeb2
-xcopy \soft\flame_API\flame_api \soft\upf\flame_api /e /y
-@CALL ng build --prod --configuration=rel --base-href /flame.kh.svc/ --deploy-url /flame.kh.svc/static/ --output-path=../upf/flame_api/static/
+xcopy \soft\flame_API\flame_api \soft\FlameDocker\flame_api /e /y
+@CALL ng build --prod --configuration=rel --base-href /flame.kh.svc/ --deploy-url /flame.kh.svc/static/ --output-path=../FlameDocker/flame_api/static/
 timeout /t 20 /nobreak > NUL
