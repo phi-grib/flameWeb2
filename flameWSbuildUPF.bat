6f309139ccb7a33ec6e25f3d95e762ce37c23916
manuel.pastor <manuel.pastor@upf.edu>
2022-03-24 19:11:42 +0100

    build for UPF

--- flameWSbuildUPF.bat
+++ flameWSbuildUPF.bat
@@ -1,4 +1,5 @@
 c:
 cd \Users\mpastor\Documents\soft\flameWeb2
 @CALL ng build --prod --configuration=build --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
+del C:\Users\mpastor\Documents\soft\flame_API\flame_api\keycloak
 timeout /t 20 /nobreak > NUL
