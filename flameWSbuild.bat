e223b5cd35f840da2ae43dda9bc4b6f9edcf5bc7
Manuel Pastor <manuel.pastor@upf.edu>
2022-03-24 08:15:27 +0100

    build bat files updated with activation/deactivation of keycloak for the backend

--- flameWSbuild.bat
+++ flameWSbuild.bat
@@ -1,4 +1,5 @@
 x:
 cd \soft\flameWeb2
 @CALL ng build --prod --configuration=build --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
+del x:\soft\flame_API\flame_api\keycloak
 timeout /t 20 /nobreak > NUL
--- flameWSbuildKey.bat
+++ flameWSbuildKey.bat
@@ -1,4 +1,5 @@
 x:
 cd \soft\flameWeb2
 @CALL ng build --prod --configuration=rel-key-local --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
+CALL >> x:\soft\flame_API\flame_api\keycloak
 timeout /t 20 /nobreak > NUL
