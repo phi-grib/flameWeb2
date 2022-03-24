de7ccae1a69c08ad185a1e370ad933665e7bd0aa
manuel.pastor <manuel.pastor@upf.edu>
2022-03-24 13:51:35 +0100

    UPF version of build

---
+++ flameWSbuildUPF.bat
@@ -0,0 +1,4 @@
+c:
+cd \Users\mpastor\Documents\soft\flameWeb2
+@CALL ng build --prod --configuration=build --deploy-url /static/ --base-Href /static/ --output-path=../flame_API/flame_api/static/
+timeout /t 20 /nobreak > NUL
